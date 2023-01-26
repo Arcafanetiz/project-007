229e6854ab501eb1a08750410b9086302d0599e8
Vibhumi-Sermsilp <vibhumi.serm@kmutt.ac.th>
2023-01-26 10:49:57 +0700

    Add project file

---
+++ [Prototype] Escape Room First Playable/Assets/Scripts/MouseCameraMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCameraMovement : MonoBehaviour
+{
+    public Vector3 originPos;
+    private Vector3 mousePos;
+
+    public float xMultiplier;
+    public float yMultiplier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        transform.position = new Vector3(originPos.x + ((mousePos.x - 0.5f) * xMultiplier), originPos.y + ((mousePos.y - 0.5f) * yMultiplier), originPos.z);
+    }
+}
