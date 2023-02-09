3308c60b030f920571bf0da4715c800f923ef735
Capt <50790210+FadeZx@users.noreply.github.com>
2023-02-09 15:59:58 +0700

    Passcode done

---
+++ [Prototype] Escape Room First Playable/Assets/Scripts/PasscodeChecker.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class PasscodeChecker : MonoBehaviour
+{
+    [SerializeField] private TMP_Text Ans;
+
+    public string Answer = "1234";
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Ans.text = "";
+    }
+    public void Number(int num)
+    {
+        Ans.text += num.ToString();
+       
+    }
+
+    public void Execute()
+    {
+        if(Ans.text == Answer)
+        {
+            Ans.text = "Correct!!!";
+        }
+        else
+        {
+            StartCoroutine(Incorrect());
+        }
+    }
+
+    private IEnumerator Incorrect()
+    {
+        Ans.text = "INCORRECT!!";
+        yield return new WaitForSeconds(2.0f);
+        Ans.text = "";
+    }
+
+    
+}
