23825db9e12cc6e5a698ae936f4a1d027c12eec8
Capt <50790210+FadeZx@users.noreply.github.com>
2023-03-14 14:41:56 +0700

    Scene Loader FadeIn&Out finish

--- [Prototype] Escape Room First Playable/Assets/scene_manager.cs
+++ [Prototype] Escape Room First Playable/Assets/scene_manager.cs
@@ -6,27 +6,39 @@ using UnityEngine.SceneManagement;
 
 public class scene_manager : MonoBehaviour
 {
-    public GameObject TransitionEffect;
+    public GameObject FadeIn;
+    public GameObject FadeOut;
     private string SceneName;
 
     void Start()
     {
-        TransitionEffect.SetActive(false);
+        FadeOut.SetActive(false);
+        FadeIn.SetActive(true);
     }
     public void LoadScene(string scenename)
     {
         SceneName = scenename;
-        TransitionEffect.SetActive(true);
+        FadeOut.SetActive(true);
         StartCoroutine(WaitForTransition()); 
     }
 
     private IEnumerator WaitForTransition()
     {
-        while (TransitionEffect.GetComponent<GDTFadeEffect>().HasFinished())
+        while (FadeOut.GetComponent<GDTFadeEffect>().HasFinished())
         {
             yield return null;
         }
         Debug.Log("Load scene: " + SceneName);
         SceneManager.LoadScene(SceneName);
     }
+    public void ExitGame()
+    {
+
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+         #else
+            Application.Quit();
+        #endif
+    
+    }       
 }
