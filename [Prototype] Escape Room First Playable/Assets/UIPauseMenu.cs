ddd8a15145bba40a856e4f4fafaf43ed0aba3f1c
Vibhumi-Sermsilp <vibhumi.serm@kmutt.ac.th>
2023-04-21 05:51:12 +0700

    Puzzle Rework

--- [Prototype] Escape Room First Playable/Assets/UIPauseMenu.cs
+++ [Prototype] Escape Room First Playable/Assets/UIPauseMenu.cs
@@ -6,6 +6,7 @@ public class UIPauseMenu : MonoBehaviour
 {
     [Header("Private Serialized Field -Do not touch-")]
     [SerializeField] private GameObject pauseMenuPanel;
+    private bool isPaused;
 
     [Header("Pause Menu Settings")]
     public KeyCode pauseKey = KeyCode.Escape;
@@ -17,33 +18,49 @@ public class UIPauseMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialize
+        SetPanelInactive(pauseMenuPanel);
+        isPaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(pauseKey))
+        if(Input.GetKeyDown(pauseKey) && !isPaused)
         {
-            if(pauseMenuPanel.activeInHierarchy == false)
-            {
-                Show();
-            }
-            else
-            {
-                Hide();
-            }
+            Show();
         }
     }
 
     public void Show()
     {
-        pauseMenuPanel.SetActive(true);
+        isPaused = true;
+        LeanTween.cancel(pauseMenuPanel);
+        SetPanelInactive(pauseMenuPanel);
+        LeanTween.alphaCanvas(pauseMenuPanel.GetComponent<CanvasGroup>(), 1.0f, duration).setEase(easeType);
+        LeanTween.scale(pauseMenuPanel.GetComponent<RectTransform>(), new Vector3(1.0f, 1.0f, 1.0f), duration).setEase(easeType).setOnComplete(() => SetPanelActive(pauseMenuPanel));
     }
 
     public void Hide()
     {
+        LeanTween.cancel(pauseMenuPanel);
+        SetPanelActive(pauseMenuPanel);
+        LeanTween.alphaCanvas(pauseMenuPanel.GetComponent<CanvasGroup>(), 0.0f, duration).setEase(easeType);
+        LeanTween.scale(pauseMenuPanel.GetComponent<RectTransform>(), new Vector3(1.5f, 1.5f, 1.5f), duration).setEase(easeType).setOnComplete(() => { SetPanelInactive(pauseMenuPanel); isPaused = false; });
+    }
 
+    private void SetPanelActive(GameObject panelGO)
+    {
+        panelGO.GetComponent<CanvasGroup>().alpha = 1;
+        panelGO.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        panelGO.GetComponent<RectTransform>().localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+
+    private void SetPanelInactive(GameObject panelGO)
+    {
+        panelGO.GetComponent<CanvasGroup>().alpha = 0;
+        panelGO.GetComponent<CanvasGroup>().blocksRaycasts = false;
+        panelGO.GetComponent<RectTransform>().localScale = new Vector3(1.5f, 1.5f, 1.5f);
     }
 
     public void changeBGM()
