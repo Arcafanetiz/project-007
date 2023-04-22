35e8cb4fbf4cdd6e0369b71f2cac2dfebea1764b
Vibhumi-Sermsilp <vibhumi.serm@kmutt.ac.th>
2023-04-23 02:08:17 +0700

    Sub room system

--- [Prototype] Escape Room First Playable/Assets/Script/System/RoomNavigation.cs
+++ [Prototype] Escape Room First Playable/Assets/Script/System/RoomNavigation.cs
@@ -5,6 +5,7 @@ using UnityEngine.Audio;
 
 public class RoomNavigation : MonoBehaviour
 {
+    public enum ViewType { STANDARD, AUXILLARY };
     public enum ViewState { PAST, PRESENT };
 
     [Header("Private Serialized Field -Do not touch-")]
@@ -12,14 +13,21 @@ public class RoomNavigation : MonoBehaviour
     [SerializeField] private int currentView;
     [SerializeField] private int previousView;
     [SerializeField] private int maxView;
+    [SerializeField] private int currentAuxView;
+    [SerializeField] private int previousAuxView;
+    [SerializeField] private int maxAuxView;
 
     [Header("Views")]
     public KeyCode nextKey = KeyCode.E;
     public KeyCode prevKey = KeyCode.Q;
+    [SerializeField] private ViewType currentType = ViewType.STANDARD;
     [SerializeField] private ViewState currentState = ViewState.PRESENT;
 
     public GameObject[] presentViews;
     public GameObject[] pastViews;
+
+    public GameObject[] presentAuxViews;
+    public GameObject[] pastAuxViews;
 
     [Header("Screen Effect")]
     public GameObject screenEffect;
@@ -43,13 +51,36 @@ public class RoomNavigation : MonoBehaviour
         }
     }
 
+    public int p_currentAuxView
+    {
+        get { return currentAuxView; }
+        set
+        {
+            if (value > maxAuxView - 1)
+            {
+                currentAuxView = 0;
+            }
+            else if (value < 0)
+            {
+                currentAuxView = maxAuxView - 1;
+            }
+            else
+            {
+                currentAuxView = value;
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
         maxView = presentViews.Length;
         currentView = 0;
-        previousView = maxView;
+        previousView = maxView - 1;
+
+        maxAuxView = presentAuxViews.Length;
+        currentAuxView = 0;
+        previousAuxView = maxAuxView - 1;
 
         presentViews[0].SetActive(true);
         pastViews[0].SetActive(false);
@@ -57,6 +88,12 @@ public class RoomNavigation : MonoBehaviour
         {
             presentViews[i].SetActive(false);
             pastViews[i].SetActive(false);
+        }
+
+        for (int i = 0; i < presentAuxViews.Length; i++)
+        {
+            presentAuxViews[i].SetActive(false);
+            pastAuxViews[i].SetActive(false);
         }
     }
 
@@ -75,20 +112,80 @@ public class RoomNavigation : MonoBehaviour
 
     public void ChangeView(int dir)
     {
-        previousView = currentView;
-        p_currentView += dir;
-        switch(currentState)
+        if(currentType == ViewType.STANDARD)
         {
-            case ViewState.PRESENT:
-                presentViews[previousView].SetActive(false);
-                presentViews[currentView].SetActive(true);
-                break;
-            case ViewState.PAST:
-                pastViews[previousView].SetActive(false);
-                pastViews[currentView].SetActive(true);
-                break;
+            if(previousView == currentView)
+            {
+                return;
+            }
+            previousView = currentView;
+            p_currentView += dir;
+            switch (currentState)
+            {
+                case ViewState.PRESENT:
+                    presentViews[previousView].SetActive(false);
+                    presentViews[currentView].SetActive(true);
+                    break;
+                case ViewState.PAST:
+                    pastViews[previousView].SetActive(false);
+                    pastViews[currentView].SetActive(true);
+                    break;
+            }
         }
-        
+        if (currentType == ViewType.AUXILLARY)
+        {
+            if(previousAuxView == currentAuxView)
+            {
+                return;
+            }
+            previousAuxView = currentAuxView;
+            p_currentAuxView += dir;
+            switch (currentState)
+            {
+                case ViewState.PRESENT:
+                    presentAuxViews[previousAuxView].SetActive(false);
+                    presentAuxViews[currentAuxView].SetActive(true);
+                    break;
+                case ViewState.PAST:
+                    pastAuxViews[previousAuxView].SetActive(false);
+                    pastAuxViews[currentAuxView].SetActive(true);
+                    break;
+            }
+        }
+    }
+
+    public void ChangeType()
+    {
+        if (currentType == ViewType.STANDARD)
+        {
+            switch (currentState)
+            {
+                case ViewState.PRESENT:
+                    presentViews[currentView].SetActive(false);
+                    presentAuxViews[0].SetActive(true);
+                    break;
+                case ViewState.PAST:
+                    pastViews[currentView].SetActive(false);
+                    pastAuxViews[0].SetActive(true);
+                    break;
+            }
+            currentType = ViewType.AUXILLARY;
+        }
+        else if (currentType == ViewType.AUXILLARY)
+        {
+            switch (currentState)
+            {
+                case ViewState.PRESENT:
+                    presentAuxViews[currentAuxView].SetActive(false);
+                    presentViews[currentView].SetActive(true);
+                    break;
+                case ViewState.PAST:
+                    pastAuxViews[currentAuxView].SetActive(false);
+                    pastViews[currentView].SetActive(true);
+                    break;
+            }
+            currentType = ViewType.STANDARD;
+        }
     }
 
     public void ToggleState()
@@ -103,22 +200,45 @@ public class RoomNavigation : MonoBehaviour
         LeanTween.scale(screenEffect, new Vector3(900.0f, 900.0f, 1.0f), 1.0f).setOnComplete(() 
             => { screenEffect.transform.localScale = Vector3.one; });
         yield return new WaitForSecondsRealtime(0.2f);
-        switch (currentState)
+        if(currentType == ViewType.STANDARD)
         {
-            case ViewState.PRESENT:
-                presentViews[currentView].SetActive(false);
-                pastViews[currentView].SetActive(true);
-                currentState = ViewState.PAST;
-                //AudioManager.instance.SwitchBGM(pastBGM);
-                LeanTween.value(gameObject, UpdateCutoffFreg, 22000.0f, 220.0f, 0.7f).setEase(LeanTweenType.easeOutExpo);
-                break;
-            case ViewState.PAST:
-                pastViews[currentView].SetActive(false);
-                presentViews[currentView].SetActive(true);
-                currentState = ViewState.PRESENT;
-                //AudioManager.instance.SwitchBGM(presentBGM);
-                LeanTween.value(gameObject, UpdateCutoffFreg, 220.0f, 22000.0f, 0.7f).setEase(LeanTweenType.easeOutExpo);
-                break;
+            switch (currentState)
+            {
+                case ViewState.PRESENT:
+                    presentViews[currentView].SetActive(false);
+                    pastViews[currentView].SetActive(true);
+                    currentState = ViewState.PAST;
+                    //AudioManager.instance.SwitchBGM(pastBGM);
+                    LeanTween.value(gameObject, UpdateCutoffFreg, 22000.0f, 500.0f, 0.7f).setEase(LeanTweenType.easeOutExpo);
+                    break;
+                case ViewState.PAST:
+                    pastViews[currentView].SetActive(false);
+                    presentViews[currentView].SetActive(true);
+                    currentState = ViewState.PRESENT;
+                    //AudioManager.instance.SwitchBGM(presentBGM);
+                    LeanTween.value(gameObject, UpdateCutoffFreg, 500.0f, 22000.0f, 0.7f).setEase(LeanTweenType.easeOutExpo);
+                    break;
+            }
+        }
+        if (currentType == ViewType.AUXILLARY)
+        {
+            switch (currentState)
+            {
+                case ViewState.PRESENT:
+                    presentAuxViews[currentAuxView].SetActive(false);
+                    pastAuxViews[currentAuxView].SetActive(true);
+                    currentState = ViewState.PAST;
+                    //AudioManager.instance.SwitchBGM(pastBGM);
+                    LeanTween.value(gameObject, UpdateCutoffFreg, 22000.0f, 500.0f, 0.7f).setEase(LeanTweenType.easeOutExpo);
+                    break;
+                case ViewState.PAST:
+                    pastAuxViews[currentAuxView].SetActive(false);
+                    presentAuxViews[currentAuxView].SetActive(true);
+                    currentState = ViewState.PRESENT;
+                    //AudioManager.instance.SwitchBGM(presentBGM);
+                    LeanTween.value(gameObject, UpdateCutoffFreg, 500.0f, 22000.0f, 0.7f).setEase(LeanTweenType.easeOutExpo);
+                    break;
+            }
         }
     }
 
